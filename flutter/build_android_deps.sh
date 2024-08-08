33d0e9e3653d22809cd6fead665bc5278e6db97a
unknown <developer@anb5.nl>
2024-08-08 12:38:53 +0200

    cat android build issue body

--- flutter/build_android_deps.sh
+++ flutter/build_android_deps.sh
@@ -67,7 +67,9 @@ function build {
   echo "*** [$ANDROID_ABI][Start] Build and install vcpkg dependencies"
   pushd "$SCRIPTDIR/.."
   $VCPKG_ROOT/vcpkg install --triplet $VCPKG_TARGET --x-install-root="$VCPKG_ROOT/installed" || true
+  echo "===BEGIN issue_body.md==="
   cat /opt/artifacts/vcpkg/installed/vcpkg/issue_body.md
+  echo "===END issue_body.md==="
   popd
   echo "*** [$ANDROID_ABI][Finished] Build and install vcpkg dependencies"
 
